c4c7f0f17350a54b67345481d9e28c01133a95d4
brwong3 <brwong3@s.sfusd.edu>
2022-07-26 23:51:08 -0700

    Installed Framer Motion

--- src/components/ItemsList.js
+++ src/components/ItemsList.js
@@ -1,5 +1,10 @@
 import React from 'react'
 
 export default function ItemsList(props) {
-
+  const table = props.item.map((item) => <h1>{item}</h1>)
+  return(
+    <div>
+      {table}
+    </div>
+  )
 }
