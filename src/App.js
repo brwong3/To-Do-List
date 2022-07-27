e424daca9e95249e110bd66cf1aa3c50b156ccaf
brwong3 <brwong3@s.sfusd.edu>
2022-07-26 22:18:45 -0700

    Finally got context working

--- src/App.js
+++ src/App.js
@@ -1,16 +1,21 @@
-import React, { createContext } from "react";
+import React, { createContext, useState } from "react";
 import Input from "./components/Input";
 import "./styles/App.css"
 
 
-export const list = createContext();
+export const listContext = createContext();
 
 function App() {
+
+  const[list, setList] = useState([]);
+
   return (
+    <listContext.Provider value={{list, setList}}>
       <div className="App">
         <h1>To-Do List</h1>
         <Input></Input>
       </div>
+    </listContext.Provider>
   );
 }
 
--- src/components/Input.js
+++ src/components/Input.js
@@ -1,13 +1,17 @@
 import React, { useState } from 'react'
 import { useContext } from 'react'
 import "../styles/Input.css"
+import { listContext } from '../App'
+
 
 export default function () {
+
+  const {list} = useContext(listContext);
 
   return (
     <div className='container'>
       <input type="text" placeholder='Water Your Dog'></input>
-      <button>Add</button>
+      <button onClick={() => console.log(list)}>Add</button>
     </div>
   )
 }
